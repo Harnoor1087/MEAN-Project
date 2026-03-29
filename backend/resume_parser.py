00953a150b9bc737dab244a03da82179c094ea80
Harnoor Singh <harnoorsinghjee@gmail.com>
2026-03-29 19:25:21 +0530

    Resolved printing text error

--- backend/resume_parser.py
+++ backend/resume_parser.py
@@ -1,10 +1,10 @@
 import pdfplumber
-pdf_path = r"C:\Users\Harnoor Singh\Downloads\Resume_Harnoor_Wipro.pdf"
+pdf_path = r"C:\Users\Harnoor Singh\Downloads\Resume_Harnoor_Best.pdf"
 def extract_text(pdf_path):
     text = ""
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
             text += page.extract_text()
-    return text
+    print (text)
 
-
+extract_text(pdf_path)
