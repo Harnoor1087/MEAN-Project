8516b06b4272829a1ae733fb4209b889e7ac59d1
Harnoor Singh <harnoorsinghjee@gmail.com>
2026-04-07 15:09:02 +0530

    Updated Candidate Model and added Job model

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,21 @@
-from sqlalchemy import Column, Integer, Float, String, Text
+from sqlalchemy import Column, Integer, String, Text, Float, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    title = Column(String)
+    description = Column(String)
+
+    mandatory_skills = Column(String)
+    optional_skills = Column(String)
+
+    certification_enabled = Column(Boolean, default=False)
+    certification_weight = Column(Float, default=0.0)
 
 class Candidate(Base):
 
@@ -20,3 +34,5 @@ class Candidate(Base):
 
     matched_skills = Column(Text)
     missing_skills = Column(Text)
+    eligibility_status = Column(String)
+    job_id = Column(Integer, ForeignKey("jobs.id"))
